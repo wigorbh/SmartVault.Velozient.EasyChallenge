3e40e5b927e6fe05fef50b59f3cf7d2b1b8045f2
wigorbh <williamigorsilva@gmail.com>
2024-06-28 13:52:48 -0300

    Firt commit: Structure

---
+++ src/TwistedFizzBuzz.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TwistedFizzBuzz.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
